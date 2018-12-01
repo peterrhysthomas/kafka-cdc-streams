0a323f9f6e0d10e8ae301e281273ea860fc0925c
peterrhysthomas@yahoo.co.uk <peterrhysthomas@yahoo.co.uk>
2018-12-01 17:59:16 +0000

    Added database create table scripts

---
+++ db-scripts/create-tables.sql
@@ -0,0 +1,4 @@
+create table posting (account_id VARCHAR(20), value_date VARCHAR(8), amount DECIMAL(10,2), insert_timestamp TIMESTAMP);
+
+create table opening_balance (account_id VARCHAR(20), value_date VARCHAR(8), opening_balance DECIMAL (10,2), insert_timestamp TIMESTAMP);
+
